210e14c0c4eb6af91c5d436f3978c789472aaedb
Isaac Thompson <shufflesninja@gmail.com>
2024-10-22 00:07:40 -0400

    Start decomp

--- src/00_Core/Map/Course.cpp
+++ src/00_Core/Map/Course.cpp
@@ -1,13 +1,71 @@
 #include "Map/Course.hpp"
 
-void Course::GetDungeonProgress(CourseProgress *param_2) {}
+void Course::GetDungeonProgress(CourseProgress *param_2)
+{
+    param_2->flags = mFlags1[0];
+        /* if course is totok */
+
+    if (mType == 3) {
+        return;
+    }
+    param_2->numKeys = (char)mNumKeys;
+    param_2->blueWarpOpen = mBlueWarpOpen;
+    return;
+}
+
+
 void Course::func_ov00_0207ca28(s32 param_2, unk32 param_3, unk32 param_4) {}
-char *Course::SetCourseName(char *src) {}
+
+char *Course::SetCourseName(char *src)
+{
+    char *pcVar1;
+  
+        /* WARNING: Could not recover jumptable at 0x0207ca70. Too many branches */
+        /* WARNING: Treating indirect jump as call */
+    pcVar1 = (char *)(*(code *)PTR_strcpy_overlay_d_0__0207ca74)(mName);
+    return pcVar1;
+}
+
 void Course::func_ov00_0207ca78(unk32 param_2, unk8 param_3, unk32 param_4) {}
 bool Course::func_ov00_0207caa8(s32 param_2, unk32 *param_3, unk8 *param_4) {}
 unk32 Course::func_ov00_0207cb30(s32 param_2) {}
-void Course::FindMapGridPos(Vec2b *pos, Course *param_2, u32 map) {}
-Vec3p *Course::FindMapCenter(unk32 map) {}
+
+void Course::FindMapGridPos(Vec2b *pos, Course *param_2, u32 map)
+{
+    byte bVar1;
+    int y;
+    Course *course_unk20;
+    int x;
+    
+    x = 0;
+    course_unk20 = param_2;
+    do {
+        y = 0;
+        do {
+        if (map == (byte)course_unk20->mMapGrid[0][y]) {
+            pos->x = (byte)x;
+            pos->y = (byte)y;
+            return;
+        }
+        y = y + 1;
+        } while (y < 10);
+        x = x + 1;
+        course_unk20 = (Course *)((int)&course_unk20->field2_0x8 + 2);
+    } while (x < 10);
+    bVar1 = (param_2->mCurrMapPos).y;
+    pos->x = (param_2->mCurrMapPos).x;
+    pos->y = bVar1;
+    return;
+}
+
+Vec3p *Course::FindMapCenter(unk32 map)
+{
+    MapData *iVar1;
+    
+    iVar1 = FindMapData(*this,map);
+    return &iVar1->mCenter;
+}
+
 void Course::func_ov00_0207cbe8(s32 param_2) {}
 unk8 Course::func_ov00_0207cc24(s32 param_2) {}
 unk32 Course::FindMapData_Unk_08(unk32 map) {}
